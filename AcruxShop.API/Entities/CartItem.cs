e3e1336d878c6f9c0c063da7229c5a387e57f863
Keith Stanley <iamstan13y@gmail.com>
2023-09-11 10:36:31 +0200

    AcruxShop.API - Added Models.

---
+++ AcruxShop.API/Entities/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AcruxShop.API.Entities;
+
+public class CartItem
+{
+    public int Id { get; set; }
+    public int CartId { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+}
