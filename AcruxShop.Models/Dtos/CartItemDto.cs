727f1981ecb30293c53f8dbc9084f1070d3dd2e4
Keith Stanley <iamstan13y@gmail.com>
2023-09-11 18:11:22 +0200

    Acrux.Models - DTO Classes.

---
+++ AcruxShop.Models/Dtos/CartItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace AcruxShop.Models.Dtos;
+
+public class CartItemDto
+{
+    public int Id { get; set; }
+    public int CartId { get; set; }
+    public int ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public string? ProductDescription { get; set; }
+    public string? ProductImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public decimal Total { get; set; }
+    public int Quantity { get; set; }
+}
