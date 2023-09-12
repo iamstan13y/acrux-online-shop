9d9b77460e8e1851ce299479da9ee403ae2f03c2
Keith Stanley <iamstan13y@gmail.com>
2023-09-12 12:55:18 +0200

    AcruxShop.API - Added Product Repository.

---
+++ AcruxShop.API/Repositories/Contracts/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using AcruxShop.API.Entities;
+
+namespace AcruxShop.API.Repositories.Contracts;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+    Task<IEnumerable<ProductCategory>> GetCategoriesAsync();
+    Task<Product> GetAsync(int id);
+    Task<ProductCategory> GetCategoryAsync(int id);
+}
