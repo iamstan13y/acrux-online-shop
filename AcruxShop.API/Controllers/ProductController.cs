57036fd67a6b172642144a97cd0a9081345870f3
Keith Stanley <iamstan13y@gmail.com>
2023-09-13 09:29:01 +0200

    AcruxShop.API - Product Controller & DTO Conversion Classes.

---
+++ AcruxShop.API/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using AcruxShop.API.Extensions;
+using AcruxShop.API.Repositories.Contracts;
+using AcruxShop.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcruxShop.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDto>>> Get()
+    {
+        try
+        {
+            var products = await _productRepository.GetAllAsync();
+            var productCategories = await _productRepository.GetCategoriesAsync();
+
+            var productDtos = products.ConvertToDto(productCategories);
+
+            return Ok(productDtos);
+        }
+
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
