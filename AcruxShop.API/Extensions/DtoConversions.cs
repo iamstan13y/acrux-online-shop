cc02c9a1b8da43aadfd5930f52ee7db40daa7eeb
Keith Stanley <iamstan13y@gmail.com>
2023-09-13 09:29:05 +0200

    AcruxShop.API - Product Controller & DTO Conversion Classes.

---
+++ AcruxShop.API/Extensions/DtoConversions.cs
@@ -0,0 +1,25 @@
+﻿using AcruxShop.API.Entities;
+using AcruxShop.Models.Dtos;
+
+namespace AcruxShop.API.Extensions;
+
+public static class DtoConversions
+{
+    public static IEnumerable<ProductDto> ConvertToDto(this IEnumerable<Product> products, IEnumerable<ProductCategory> productCategories)
+    {
+        return (from product in products
+                join productCategory in productCategories
+                on product.CategoryId equals productCategory.Id
+                select new ProductDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    ImageUrl = product.ImageUrl,
+                    Price = product.Price,
+                    Quantity = product.Quantity,
+                    CategoryId = product.CategoryId,
+                    CategoryName = productCategory.Name
+                }).ToList();
+    }
+}
