f32c9a7240a6ea61b835891ea472bba87b874190
Keith Stanley <iamstan13y@gmail.com>
2023-09-13 09:29:09 +0200

    AcruxShop.API - Product Controller & DTO Conversion Classes.

--- AcruxShop.API/Program.cs
+++ AcruxShop.API/Program.cs
@@ -1,10 +1,14 @@
 using AcruxShop.API.Data;
+using AcruxShop.API.Repositories;
+using AcruxShop.API.Repositories.Contracts;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContextPool<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 
