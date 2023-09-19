1cfeabb747e3d232f92908ea6c559fc66e65f5ba
Keith Stanley <iamstan13y@gmail.com>
2023-09-19 14:59:58 +0200

    AcruxShop.Web - Display Product Data to User - I.

---
+++ AcruxShop.Web/Pages/ProductsBase.cs
@@ -0,0 +1,19 @@
+﻿using AcruxShop.Models.Dtos;
+using AcruxShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace AcruxShop.Web.Pages;
+
+public class ProductsBase : ComponentBase
+{
+    [Inject]
+    public IProductService ProductService { get; set; }
+
+    public IEnumerable<ProductDto>? Products { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Products = await ProductService.GetAllAsync();
+    }
+
+}
