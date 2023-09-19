6d901a3e89b433a861e1c57621e799df85ec96ee
Keith Stanley <iamstan13y@gmail.com>
2023-09-19 15:00:09 +0200

    AcruxShop.Web - Display Product Data to User - I.

--- AcruxShop.Web/Program.cs
+++ AcruxShop.Web/Program.cs
@@ -1,4 +1,6 @@
 using AcruxShop.Web;
+using AcruxShop.Web.Services;
+using AcruxShop.Web.Services.Contracts;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7254/") });
+
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
