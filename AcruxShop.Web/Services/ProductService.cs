5dca35e4b8fdad1af434f61f317d26847f101919
Keith Stanley <iamstan13y@gmail.com>
2023-09-19 15:00:13 +0200

    AcruxShop.Web - Display Product Data to User - I.

---
+++ AcruxShop.Web/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using AcruxShop.Models.Dtos;
+using AcruxShop.Web.Services.Contracts;
+using System.Net.Http.Json;
+
+namespace AcruxShop.Web.Services;
+
+public class ProductService : IProductService
+{
+    private readonly HttpClient _httpClient;
+
+    public ProductService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<ProductDto>> GetAllAsync()
+    {
+        try
+        {
+            var products = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/Product");
+
+            return products;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
