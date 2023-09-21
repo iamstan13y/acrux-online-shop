ae7643fccc8a4b44afc809d49f378dccea87f540
Keith Stanley <iamstan13y@gmail.com>
2023-09-21 09:20:52 +0200

    AcruxShop.Web - Display Products #1.

---
+++ AcruxShop.Web/Pages/DisplayProductBase.cs
@@ -0,0 +1,11 @@
+﻿using AcruxShop.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace AcruxShop.Web.Pages;
+
+public class DisplayProductBase : ComponentBase
+{
+
+    [Parameter]
+    public IEnumerable<ProductDto> Products { get; set; }
+}
