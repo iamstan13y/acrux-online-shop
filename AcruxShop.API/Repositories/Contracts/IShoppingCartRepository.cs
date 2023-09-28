5f7aa56ce73251a6c1b0cd85775b1a82018973ec
Keith Stanley <iamstan13y@gmail.com>
2023-09-28 16:26:53 +0200

    Add Product to Shopping Cart - I.

---
+++ AcruxShop.API/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,13 @@
+﻿using AcruxShop.API.Entities;
+using AcruxShop.Models.Dtos;
+
+namespace AcruxShop.API.Repositories.Contracts;
+
+public interface IShoppingCartRepository
+{
+    Task<CartItem> AddItemAsync(CartItemToAddDto cartItemToAddDto);
+    Task<CartItem> UpdateQuantityAsync(int id, CartItemQuantityUpdateDto quantityUpdateDto);
+    Task<CartItem> DeleteItemAsync(int id);
+    Task<CartItem> GetItemAsync(int id);
+    Task<IEnumerable<CartItem>> GetItemsAsync(int userId);
+}
