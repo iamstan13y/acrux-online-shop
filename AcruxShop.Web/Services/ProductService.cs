650aee28edbc92a0848bd8cccc6cbbcefa99eceb
Keith Stanley <iamstan13y@gmail.com>
2023-09-28 15:59:13 +0200

    Display Specific Product Information.

--- AcruxShop.Web/Services/ProductService.cs
+++ AcruxShop.Web/Services/ProductService.cs
@@ -1,5 +1,6 @@
 ﻿using AcruxShop.Models.Dtos;
 using AcruxShop.Web.Services.Contracts;
+using System.Net;
 using System.Net.Http.Json;
 
 namespace AcruxShop.Web.Services;
@@ -20,6 +21,35 @@ public class ProductService : IProductService
             var products = await _httpClient.GetFromJsonAsync<IEnumerable<ProductDto>>("api/v1/Product");
 
             return products;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    public async Task<ProductDto> GetAsync(int id)
+    {
+        try
+        {
+            var response = await _httpClient.GetAsync($"api/v1/Product/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                if (response.StatusCode == HttpStatusCode.NoContent)
+                {
+                    return default(ProductDto);
+                }
+
+                return await response.Content.ReadFromJsonAsync<ProductDto>();
+            }
+            else
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                throw new Exception(message); 
+            }
+
         }
         catch (Exception)
         {
