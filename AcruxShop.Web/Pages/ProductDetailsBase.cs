61f8bf7397a865f7f0fa47863cc8f2b52951f717
Keith Stanley <iamstan13y@gmail.com>
2023-09-28 15:58:49 +0200

    Display Specific Product Information.

---
+++ AcruxShop.Web/Pages/ProductDetailsBase.cs
@@ -0,0 +1,27 @@
+﻿using AcruxShop.Models.Dtos;
+using AcruxShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace AcruxShop.Web.Pages;
+
+public class ProductDetailsBase : ComponentBase
+{
+    [Parameter]
+    public int Id { get; set; }
+    [Inject]
+    public IProductService? ProductService { get; set; }
+    public ProductDto? Product { get; set; }
+    public string? ErrorMessage { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            Product = await ProductService!.GetAsync(Id);
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = ex.Message;
+        }
+    }
+}
