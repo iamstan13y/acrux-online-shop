7325791434af778e0d8f6bd33de83d19196a4389
Keith Stanley <iamstan13y@gmail.com>
2023-10-12 18:34:10 +0200

    Add Product to Shopping Cart #1.

--- AcruxShop.API/Repositories/ShoppingCartRepository.cs
+++ AcruxShop.API/Repositories/ShoppingCartRepository.cs
@@ -1,14 +1,43 @@
-﻿using AcruxShop.API.Entities;
+﻿using AcruxShop.API.Data;
+using AcruxShop.API.Entities;
 using AcruxShop.API.Repositories.Contracts;
 using AcruxShop.Models.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace AcruxShop.API.Repositories;
 
 public class ShoppingCartRepository : IShoppingCartRepository
 {
-    public Task<CartItem> AddItemAsync(CartItemToAddDto cartItemToAddDto)
+    private readonly AppDbContext _context;
+
+    public ShoppingCartRepository(AppDbContext context) => _context = context;
+
+    private async Task<bool> CartItemExistsAsync(int cartId, int productId) =>
+        await _context.CartItems!.AnyAsync(c => c.CartId == cartId && c.ProductId == productId);
+
+    public async Task<CartItem> AddItemAsync(CartItemToAddDto cartItemToAddDto)
     {
-        throw new NotImplementedException();
+        if (await CartItemExistsAsync(cartItemToAddDto.CartId, cartItemToAddDto.ProductId) == false)
+        {
+            var item = await (from product in _context.Products
+                              where product.Id == cartItemToAddDto.ProductId
+                              select new CartItem
+                              {
+                                  CartId = cartItemToAddDto.CartId,
+                                  ProductId = product.Id,
+                                  Quantity = cartItemToAddDto.Quantity
+                              }).SingleOrDefaultAsync();
+
+            if (item != null)
+            {
+                var result = await _context.CartItems!.AddAsync(item);
+                await _context.SaveChangesAsync();
+
+                return result.Entity;
+            }
+        }
+
+        return null;
     }
 
     public Task<CartItem> DeleteItemAsync(int id)
@@ -16,14 +45,34 @@ public class ShoppingCartRepository : IShoppingCartRepository
         throw new NotImplementedException();
     }
 
-    public Task<CartItem> GetItemAsync(int id)
+    public async Task<CartItem> GetItemAsync(int id)
     {
-        throw new NotImplementedException();
+        return await (from cart in _context.Carts
+                      join cartItem in _context.CartItems
+                      on cart.Id equals cartItem.CartId
+                      where cartItem.Id == id
+                      select new CartItem
+                      {
+                          Id = cartItem.Id,
+                          ProductId = cartItem.ProductId,
+                          Quantity = cartItem.Quantity,
+                          CartId = cartItem.CartId
+                      }).FirstOrDefaultAsync();
     }
 
-    public Task<IEnumerable<CartItem>> GetItemsAsync(int userId)
+    public async Task<IEnumerable<CartItem>> GetItemsAsync(int userId)
     {
-        throw new NotImplementedException();
+        return await (from cart in _context.Carts
+                      join cartItem in _context.CartItems!
+                      on cart.Id equals cartItem.Id
+                      where cart.UserId == userId
+                      select new CartItem
+                      {
+                          Id = cartItem.Id,
+                          ProductId = cartItem.ProductId,
+                          Quantity = cartItem.Quantity,
+                          CartId = cartItem.CartId
+                      }).ToListAsync();
     }
 
     public Task<CartItem> UpdateQuantityAsync(int id, CartItemQuantityUpdateDto quantityUpdateDto)
