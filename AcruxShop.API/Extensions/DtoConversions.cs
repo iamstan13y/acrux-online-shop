ae798886b48ddbfaad27dc3e5be8e71fae2090d7
Keith Stanley <iamstan13y@gmail.com>
2023-10-12 18:34:06 +0200

    Add Product to Shopping Cart #1.

--- AcruxShop.API/Extensions/DtoConversions.cs
+++ AcruxShop.API/Extensions/DtoConversions.cs
@@ -37,4 +37,39 @@ public static class DtoConversions
             CategoryName = productCategory.Name
         };
     }
+
+    public static IEnumerable<CartItemDto> ConvertToDto(this IEnumerable<CartItem> cartItems, IEnumerable<Product> products)
+    {
+        return (from cartItem in cartItems
+                join product in products
+                on cartItem.ProductId equals product.Id
+                select new CartItemDto
+                {
+                    CartId = cartItem.CartId,
+                    ProductId = cartItem.ProductId,
+                    ProductName = product.Name,
+                    ProductDescription = product.Description,
+                    ProductImageUrl = product.ImageUrl,
+                    Price= product.Price,
+                    Id = cartItem.Id,
+                    Quantity= cartItem.Quantity,
+                    Total = product.Price * cartItem.Quantity,
+                }).ToList();
+    }
+
+    public static CartItemDto ConvertToDto(this CartItem cartItem, Product product)
+    {
+        return new CartItemDto
+                {
+                    CartId = cartItem.CartId,
+                    ProductId = cartItem.ProductId,
+                    ProductName = product.Name,
+                    ProductDescription = product.Description,
+                    ProductImageUrl = product.ImageUrl,
+                    Price = product.Price,
+                    Id = cartItem.Id,
+                    Quantity = cartItem.Quantity,
+                    Total = product.Price * cartItem.Quantity,
+                };
+    }
 }
