4119723c0ca16a116b8ab9ecf3dfc69dcfdb433d
Keith Stanley <iamstan13y@gmail.com>
2023-10-12 18:34:03 +0200

    Add Product to Shopping Cart #1.

---
+++ AcruxShop.API/Controllers/ShoppingCartController.cs
@@ -0,0 +1,63 @@
+﻿using AcruxShop.API.Extensions;
+using AcruxShop.API.Repositories.Contracts;
+using AcruxShop.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcruxShop.API.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class ShoppingCartController : ControllerBase
+{
+    private readonly IShoppingCartRepository _shoppingCartRepository;
+    private readonly IProductRepository _productRepository;
+
+    public ShoppingCartController(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository)
+    {
+        _shoppingCartRepository = shoppingCartRepository;
+        _productRepository = productRepository;
+    }
+
+    [HttpGet("items/{userId}")]
+    public async Task<ActionResult<IEnumerable<CartItemDto>>> Get(int userId)
+    {
+        try
+        {
+            var cartItems = await _shoppingCartRepository.GetItemsAsync(userId);
+
+            if (cartItems == null) return NoContent();
+
+            var products = await _productRepository.GetAllAsync() ?? throw new Exception("No products in system.");
+
+            var cartItemsDto = cartItems.ConvertToDto(products);
+
+            return Ok(cartItemsDto);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Some error occured: {ex.Message}");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CartItemDto>> GetItem(int id)
+    {
+        try
+        {
+            var cartItem = await _shoppingCartRepository.GetItemAsync(id);
+            if (cartItem == null) return NotFound();
+
+            var product = await _productRepository.GetAsync(cartItem.ProductId);
+            if (product == null) return NotFound();
+
+            var cartItemDto = cartItem.ConvertToDto(product);
+
+            return Ok(cartItemDto);
+
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Some error occured: {ex.Message}");
+        }
+    }
+}
