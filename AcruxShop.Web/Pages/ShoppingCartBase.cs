7551c16aefce3db97c8fa641d02c28ffe72f2206
Keith Stanley <iamstan13y@gmail.com>
2023-10-15 17:36:40 +0200

    Add Products to Shopping Cart.

---
+++ AcruxShop.Web/Pages/ShoppingCartBase.cs
@@ -0,0 +1,25 @@
+﻿using AcruxShop.Models.Dtos;
+using AcruxShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace AcruxShop.Web.Pages;
+
+public class ShoppingCartBase : ComponentBase
+{
+    [Inject]
+    public IShoppingCartService? ShoppingCartService { get; set; }
+    public IEnumerable<CartItemDto>? CartItems { get; set; }
+	public string? ErrorMessage { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+		try
+		{
+			CartItems = await ShoppingCartService!.GetCartItemsAsync(HardCoded.UserId);
+		}
+		catch (Exception ex)
+		{
+		 ErrorMessage = ex.Message;
+		}
+    }
+}
