b1d1f1744c855f9d38e2ea827aa178c48740a075
Keith Stanley <iamstan13y@gmail.com>
2023-10-15 17:36:44 +0200

    Add Products to Shopping Cart.

---
+++ AcruxShop.Web/Services/Contracts/IShoppingCartService.cs
@@ -0,0 +1,9 @@
+﻿using AcruxShop.Models.Dtos;
+
+namespace AcruxShop.Web.Services.Contracts;
+
+public interface IShoppingCartService
+{
+    Task<IEnumerable<CartItemDto>> GetCartItemsAsync(int userId);
+    Task<CartItemDto> AddItemAsync(CartItemToAddDto cartItemToAddDto);
+}
