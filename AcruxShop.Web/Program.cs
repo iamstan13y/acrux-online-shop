d2b2be87576e41759ad486a175dcc8f92b2873d2
Keith Stanley <iamstan13y@gmail.com>
2023-10-15 17:36:53 +0200

    Add Products to Shopping Cart.

--- AcruxShop.Web/Program.cs
+++ AcruxShop.Web/Program.cs
@@ -11,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7254/") });
 
 builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
 
 await builder.Build().RunAsync();
