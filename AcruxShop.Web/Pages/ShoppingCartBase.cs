3d8f76649f0d49723b386a17396789eb79ba9a90
Keith Stanley <iamstan13y@gmail.com>
2023-10-16 09:03:34 +0200

    Remove Product from Shopping Cart.

--- AcruxShop.Web/Pages/ShoppingCartBase.cs
+++ AcruxShop.Web/Pages/ShoppingCartBase.cs
@@ -8,18 +8,34 @@ public class ShoppingCartBase : ComponentBase
 {
     [Inject]
     public IShoppingCartService? ShoppingCartService { get; set; }
-    public IEnumerable<CartItemDto>? CartItems { get; set; }
+    public List<CartItemDto>? CartItems { get; set; }
 	public string? ErrorMessage { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
 		try
 		{
-			CartItems = await ShoppingCartService!.GetCartItemsAsync(HardCoded.UserId);
+			CartItems = (await ShoppingCartService!.GetCartItemsAsync(HardCoded.UserId)).ToList();
 		}
 		catch (Exception ex)
 		{
 		 ErrorMessage = ex.Message;
 		}
     }
+
+	protected async Task DeleteCartItem_Click(int id)
+	{
+		var cartitemDto = await ShoppingCartService.DeleteItemAsync(id);
+
+		RemoveCartItem(id);
+	}
+
+    private CartItemDto GetCartItem(int id) => CartItems?.FirstOrDefault(i => i.Id == id)!;
+
+    private void RemoveCartItem(int id)
+	{
+		var cartItemDto = GetCartItem(id);
+
+		CartItems?.Remove(cartItemDto);
+	}
 }
