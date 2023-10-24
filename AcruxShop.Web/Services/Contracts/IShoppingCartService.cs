99b9ac6d274cac3d9417e4036bd2aaae8fb8d5de
Keith Stanley <iamstan13y@gmail.com>
2023-10-24 19:01:54 +0200

    Update the Header Menu in Response to a Change to the State of the Shopping Cart (Creating Custom Events in Blazor).

--- AcruxShop.Web/Services/Contracts/IShoppingCartService.cs
+++ AcruxShop.Web/Services/Contracts/IShoppingCartService.cs
@@ -8,4 +8,7 @@ public interface IShoppingCartService
     Task<CartItemDto> AddItemAsync(CartItemToAddDto cartItemToAddDto);
     Task<CartItemDto> DeleteItemAsync(int id);
     Task<CartItemDto> UpdateQuantityAsync(CartItemQuantityUpdateDto quantityUpdateDto);
+
+    event Action<int> OnShoppingCartChanged;
+    void RaiseEventOnShoppingCartChanged(int totalQuantity);
 }
