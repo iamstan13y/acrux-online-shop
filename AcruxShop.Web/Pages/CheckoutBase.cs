1c5ad2d8d404b63df98737370305e76a5d7cc683
Keith Stanley <iamstan13y@gmail.com>
2023-12-01 18:16:49 +0200

    Integrate PayPal - WIP.

--- AcruxShop.Web/Pages/CheckoutBase.cs
+++ AcruxShop.Web/Pages/CheckoutBase.cs
@@ -1,6 +1,38 @@
-﻿namespace AcruxShop.Web.Pages
+﻿using AcruxShop.Models.Dtos;
+using AcruxShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace AcruxShop.Web.Pages
 {
-    public class CheckoutBase
+    public class CheckoutBase : ComponentBase
     {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+        protected IEnumerable<CartItemDto> CartItems { get; set; }
+        protected int TotalQuantity { get; set; }
+        protected string PaymentDescription { get; set; }
+        protected decimal PaymentAmount { get; set; }
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                CartItems = await ShoppingCartService.GetCartItemsAsync(HardCoded.UserId);
+
+                if (CartItems != null)
+                {
+                    Guid orderGuid = Guid.NewGuid();
+                    PaymentAmount = CartItems.Sum
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
