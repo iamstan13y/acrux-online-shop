2b7c1e74e062d181f94fe83ccf1501686410b149
Keith Stanley <iamstan13y@gmail.com>
2023-12-03 17:38:49 +0200

    Dynamically Populate Side Bar Menu.

---
+++ AcruxShop.Models/Dtos/ProductCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AcruxShop.Models.Dtos;
+
+public class ProductCategoryDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? IconCSS { get; set; }
+}
