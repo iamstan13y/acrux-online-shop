086be1ca6731875b9d2b6de967ba42a15a50d3aa
Keith Stanley <iamstan13y@gmail.com>
2023-12-03 17:38:38 +0200

    Dynamically Populate Side Bar Menu.

---
+++ AcruxShop.API/Migrations/20231203131552_ProductCategoryIconsToDb.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AcruxShop.API.Migrations
+{
+    public partial class ProductCategoryIconsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IconCSS",
+                table: "ProductCategories",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IconCSS",
+                value: "fas fa-spa");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IconCSS",
+                value: "fas fa-couch");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "IconCSS",
+                value: "fas fa-headphones");
+
+            migrationBuilder.UpdateData(
+                table: "ProductCategories",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "IconCSS",
+                value: "fas fa-shoe-prints");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IconCSS",
+                table: "ProductCategories");
+        }
+    }
+}
