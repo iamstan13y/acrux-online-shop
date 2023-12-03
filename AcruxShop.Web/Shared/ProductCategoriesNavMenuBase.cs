206b0c44293d58f8c01010b6077829f5c5ef668a
Keith Stanley <iamstan13y@gmail.com>
2023-12-03 17:39:04 +0200

    Dynamically Populate Side Bar Menu.

---
+++ AcruxShop.Web/Shared/ProductCategoriesNavMenuBase.cs
@@ -0,0 +1,25 @@
+﻿using AcruxShop.Models.Dtos;
+using AcruxShop.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+
+namespace AcruxShop.Web.Shared;
+
+public class ProductCategoriesNavMenuBase : ComponentBase
+{
+    [Inject]
+    public IProductService? ProductService { get; set; }
+    public IEnumerable<ProductCategoryDto>? ProductCategoryDtos { get; set; }
+    public string? ErrorMessage { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            ProductCategoryDtos = await ProductService.GetProductCategories();
+        }
+        catch (Exception ex)
+        {
+            ErrorMessage = ex.Message;
+        }
+    }
+}
