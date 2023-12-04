f81dc56d4af26ce97f2a508977591ba0d2c56a49
Keith Stanley <iamstan13y@gmail.com>
2023-12-04 18:32:39 +0200

    Dynamically Populate Side Bar Menu - II.

--- AcruxShop.Web/Services/Contracts/IProductService.cs
+++ AcruxShop.Web/Services/Contracts/IProductService.cs
@@ -7,4 +7,5 @@ public interface IProductService
     Task<IEnumerable<ProductDto>> GetAllAsync();
     Task<ProductDto> GetAsync(int id);
     Task<IEnumerable<ProductCategoryDto>> GetProductCategories();
+    Task<IEnumerable<ProductDto>> GetItemsByCategory(int categoryId);
 }
