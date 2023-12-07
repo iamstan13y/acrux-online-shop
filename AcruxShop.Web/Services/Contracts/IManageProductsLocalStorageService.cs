04c56d37c84c1f5b9f4169e82e5b31d3e990b72e
Keith Stanley <iamstan13y@gmail.com>
2023-12-07 11:55:06 +0200

    Manage Products Local Storage Service.

---
+++ AcruxShop.Web/Services/Contracts/IManageProductsLocalStorageService.cs
@@ -0,0 +1,9 @@
+﻿using AcruxShop.Models.Dtos;
+
+namespace AcruxShop.Web.Services.Contracts;
+
+public interface IManageProductsLocalStorageService
+{
+    Task<IEnumerable<ProductDto>> GetCollectionAsync();
+    Task RemoveCollection();
+} 
