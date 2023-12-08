57cabe5e639815d4f74b775dd6f604f28192a7cf
Keith Stanley <iamstan13y@gmail.com>
2023-12-08 16:55:15 +0200

    Optimizations - Manage Products Local Storage Service.

--- AcruxShop.Web/Program.cs
+++ AcruxShop.Web/Program.cs
@@ -1,6 +1,7 @@
 using AcruxShop.Web;
 using AcruxShop.Web.Services;
 using AcruxShop.Web.Services.Contracts;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -10,7 +11,12 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7254/") });
 
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
+
+builder.Services.AddScoped<IManageProductsLocalStorageService, ManageProductsLocalStorageService>();
+builder.Services.AddScoped<IManageCartItemsLocalStorageService, ManageCartItemsLocalStorageService>();
 
 await builder.Build().RunAsync();
