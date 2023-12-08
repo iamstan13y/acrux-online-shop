37c4fe0e1cb02b19e9ca22d1d76005fe4aa55e08
Keith Stanley <iamstan13y@gmail.com>
2023-12-08 16:55:09 +0200

    Optimizations - Manage Products Local Storage Service.

---
+++ AcruxShop.Web/Services/Contracts/IManageCartItemsLocalStorageService.cs
@@ -0,0 +1,10 @@
+﻿using AcruxShop.Models.Dtos;
+
+namespace AcruxShop.Web.Services.Contracts;
+
+public interface IManageCartItemsLocalStorageService
+{
+    Task<List<CartItemDto>> GetCollectionAsync();
+    Task SaveCollection(List<CartItemDto> cartItemDtos);
+    Task RemoveCollection();
+} 
